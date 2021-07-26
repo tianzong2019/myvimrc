d6e353f41bda6b715f58bc15785e4d34a87960cd
tianzong2019 <59113990+tianzong2019@users.noreply.github.com>
2021-07-26 12:15:08 +0800

    Update env.sh

--- backup/env.sh
+++ backup/env.sh
@@ -12,19 +12,22 @@ function  croot(){
 function cyimg(){
 	cpath=`pwd`
 	cdirName=`basename ${cpath}`
+	tftDir="/home/albter/devnet/tftp"
+	nfsDir="/home/albter/devnet/nfs"
+	logDir="/home/albter/net/logdir"
 	echo "当前目录："${cdirName}
 	if [[ $cdirName == *kernel* ]]
 	then
-		tftDir="/home/albter/Documents/zScript/devTftp"
 		rm -rf ${tftDir}/*
 		cp ${rdir}/arch/arm64/boot/Image ${tftDir}
-		cp ${rdir}/arch/arm64/boot/dts/horizon/hobot-j3-fpga.dtb ${tftDir}
-		#cp `find ${rdir}/drivers/ips -name "*.ko"` ~/Documents/net/devnfs/test/bin
+		cp ${rdir}/arch/arm64/boot/dts/horizon/hobot-j5-fpga-camera.dtb ${tftDir}
+		# cp ${rdir}/arch/arm64/boot/dts/horizon/hobot-j3-fpga.dtb ${tftDir}
+		#cp `find ${rdir}/drivers/ips -name "*.ko"` ${nfsDir}/test/bin
 	elif [[ $cdirName == *u-boot* ]]
 	then
 		if [[ ${serverHost} == "fpga" ]]
 		then
-			#sshpass -p 'hori123zon' scp -r u-boot.bin shlabpc01@10.106.32.14:~/xiaobo/workspace/image
+			sshpass -p '888888' ssh -t xiaobo@10.106.32.14 "rm -rf ~/Documents/image/u-boot.bin" >/dev/null 2>&1
 			sshpass -p '888888' scp -r u-boot.bin xiaobo@10.106.32.14:~/Documents/image
 		elif [[ ${serverHost} == "haps" ]]
 		then
@@ -33,14 +36,14 @@ function cyimg(){
 	elif [[ $cdirName == *testbench* ]]
 	then
 		echo "在这里添加需要copy的命令"
-		rm -rf ../net/devnfs/*
-		cp -r ${rdir}/install/* ../net/devnfs
+		rm -rf ${nfsDir}/*
+		cp -r ${rdir}/install/* ${nfsDir}
 	else
 	  echo "请检查当前所在路径："${cpath}
 	fi
 }
 
 function cpylog(){
-	sshpass -p '888888' scp -r xiaobo@10.106.32.14:~/minicom.cap ~/Documents/net/logdir/log-$(date "+%Y%m%d_%H%M%S").txt
+	sshpass -p '888888' scp -r xiaobo@10.106.32.14:~/minicom.cap ${logDir}/log-$(date "+%Y%m%d_%H%M%S").txt
 	sshpass -p '888888' ssh -t xiaobo@10.106.32.14 "rm -rf ~/minicom.cap; touch ~/minicom.cap" >/dev/null 2>&1
 }
