be033194a46832d0ad851e37c80f775249cca55c
tianzong2019 <59113990+tianzong2019@users.noreply.github.com>
2022-08-01 11:36:52 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,8 +8,8 @@ tempdir=${scdir}/.temp
 
 rm -rf ${tempdir}
 mkdir -p ${tempdir}/vim
-cp ${scdir}/toolset/basic.txt ${tempdir}/vimrc
-cp ${scdir}/toolset/cus.clang-format ${HOME}/cus.clang-format
+cp -rf ${scdir}/toolset/basic.txt ${tempdir}/vimrc
+cp -rf ${scdir}/toolset/cus.clang-format ${HOME}/cus.clang-format
 
 #### 插件列表 ###########################################################
 #  bufexplorer-7.4.21
